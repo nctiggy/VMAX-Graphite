c44bfafcf7abf9782664902fe0819d7e875ceab2
nctiggy <craig.j.smith@emc.com>
2015-10-04 16:13:29 -0700

    Added Graphite Metric push

--- collector.rb
+++ collector.rb
@@ -1,6 +1,7 @@
 require "rest_client"
 require "json"
 require "base64"
+%w{simple-graphite}.each { |l| require l }
 
 @timestamp=''
 settings_file="settings.json"
@@ -37,10 +38,12 @@ end
 
 config=readSettings(settings_file)
 auth = Base64.strict_encode64("#{config['uni_user']}:#{config['uni_password']}")
+g = Graphite.new({host: config['graphite_host'], port: config['graphite_port']})
 
 config['unisphere'].each do |unisphere|
 	unisphere['symmetrix'].each do |symmetrix|
 		config['monitor'].each do |monitor|
+			payload = {}
 			###arrays_timestamp = JSON.parse('{"arrayKeyResult": {"arrayInfo": [{"symmetrixId": "000192601775","lastAvailableDate": "1441310100000"}]}}')
 			rest_get("https://#{unisphere['ip']}:#{unisphere['port']}/unimax/restapi/#{monitor['type']}/#{monitor['scope']}/keys", auth)['arrayKeyResult']['arrayInfo']
 			arrays_timestamp['arrayKeyResult']['arrayInfo'].each do |array|
@@ -49,7 +52,10 @@ config['unisphere'].each do |unisphere|
 				end
 			end
 			payload = {arrayParam: {symmetrixId: symmetrix['sid'], startDate: @timestamp, endDate: @timestamp, metrics: monitor['metrics']}}
-			rest_post(payload, "https://#{unisphere['ip']}:#{unisphere['port']}/unimax/restapi/#{monitor['type']}/#{monitor['scope']}/metrics", auth)
+			metrics = rest_post(payload, "https://#{unisphere['ip']}:#{unisphere['port']}/unimax/restapi/#{monitor['type']}/#{monitor['scope']}/metrics", auth)
+			monitor['metrics'].each do |metric|
+				payload["symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{metric}] = metrics['iterator']['resultList']['result'][0][metric]
+			g.send_metrics(payload)
 		end
 	end
 end
