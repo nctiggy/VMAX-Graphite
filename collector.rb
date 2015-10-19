64cdecc48eb7281c0adc746e10220256dc38bb4a
nctiggy <craig.j.smith@emc.com>
2015-10-19 16:09:13 -0700

    add version compat

--- collector.rb
+++ collector.rb
@@ -10,6 +10,9 @@ require "pry-byebug"
 
 settings_file="settings.json"
 
+####################################################################################
+# Method: Read's the Unisphere XSD file and gets all Metrics for the specified scope
+####################################################################################
 def get_metrics(param_type,xsd)
 	output = Array.new
 	JSON.parse(xsd)['xs:schema']['xs:simpleType'].each do |type|
@@ -22,34 +25,56 @@ def get_metrics(param_type,xsd)
 	return output
 end
 
+#####################################
+# Method: Reutrns keys for all scopes
+#####################################
 def get_keys(unisphere,symmetrix,monitor,auth)
 	if monitor['scope'].downcase == "array"
-		rest = rest_get("https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/keys", auth)["#{monitor['scope'].downcase}Info"]
+		rest = rest_get("https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/keys", auth)
 	else
-		payload = { "#{monitor['scope']}KeyParam" => { "symmetrixId" => symmetrix['sid']}}
-		rest = rest_post(payload.to_json,"https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/keys", auth)["#{monitor['scope'].downcase}KeyResult"]["#{monitor['scope'].downcase}Info"]
+		payload = { "#{monitor['scope']}KeyParam" => { "symmetrixId" => symmetrix['sid']}} if unisphere['version'] == 7
+		payload = { "symmetrixId" => symmetrix['sid']} if unisphere['version'] == 8
+		rest = rest_post(payload.to_json,"https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/keys", auth)
 	end
 	output = rest["#{monitor['scope'].downcase}Info"] if unisphere['version'] == 8
 	output = rest["#{monitor['scope'].downcase}KeyResult"]["#{monitor['scope'].downcase}Info"] if unisphere['version'] == 7
 	return output
 end
 
+##################################################
+# Method: Returns Metrics for all component scopes
+##################################################
 def get_component_metrics(unisphere,symmetrix,monitor,key,metrics,auth)
 	componentId = get_component_id_key(monitor['scope'])
-	payload = { "#{monitor['scope']}Param" => { "symmetrixId" => symmetrix['sid'], "startDate" => key['lastAvailableDate'], "endDate" => key['lastAvailableDate'], "#{componentId}Id" => key["#{componentId}Id"], "metrics" => metrics}}
-	rest = rest_post(payload.to_json,"https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/metrics", auth)['resultList']['result'][0]
+	payload = { "#{componentId}Param" => { "symmetrixId" => symmetrix['sid'], "startDate" => key['lastAvailableDate'], "endDate" => key['lastAvailableDate'], "#{componentId}Id" => key["#{componentId}Id"], "metrics" => metrics}} if unisphere['version'] == 7
+	payload = { "symmetrixId" => symmetrix['sid'], "dataFormat" => "Average", "startDate" => key['lastAvailableDate'], "endDate" => key['lastAvailableDate'], "#{componentId}Id" => key["#{componentId}Id"], "metrics" => metrics} if unisphere['version'] == 8
+	rest = rest_post(payload.to_json,"https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/metrics", auth)
 	output = rest['resultList']['result'][0] if unisphere['version'] == 8
 	output = rest['iterator']['resultList']['result'][0] if unisphere['version'] == 7
 	return output
 end
 
+##################################################
+# Method: Returns Metrics for the Array scope only
+##################################################
 def get_array_metrics(unisphere,symmetrix,monitor,key,metrics,auth)
-	payload = { "metrics" => metrics, "dataFormat" => "Average", "symmetrixId" => symmetrix['sid'], "startDate" => key['lastAvailableDate'], "endDate" => key['lastAvailableDate']}
+	### Create a payload if unisphere 7 ###
+	payload = { "#{get_component_id_key(monitor['scope'])}Param" => { "symmetrixId" => symmetrix['sid'], "startDate" => key['lastAvailableDate'], "endDate" => key['lastAvailableDate'], "metrics" => metrics}} if unisphere['version'] == 7
+	### Create a payload if unisphere 8 ###
+	payload = { "metrics" => metrics, "dataFormat" => "Average", "symmetrixId" => symmetrix['sid'], "startDate" => key['lastAvailableDate'], "endDate" => key['lastAvailableDate']} if unisphere['version'] == 8
+	### Make the rest call to unisphere ###
 	rest = rest_post(payload.to_json, "https://#{unisphere['ip']}:#{unisphere['port']}/univmax/restapi/#{monitor['type']}/#{monitor['scope']}/metrics", auth)['resultList']['result'][0]
+	### Parse the results to return an array of metrics for unisphere 8 ###
 	output = rest['resultList']['result'][0] if unisphere['version'] == 8
+	### Parse the results to return an array of metrics for unisphere 7 ###
 	output = rest['iterator']['resultList']['result'][0] if unisphere['version'] == 7
+	### Return the array of Metrics ###
+	return output
 end
 
+#########################
+# Method: API Post Method
+#########################
 def rest_post(payload, api_url, auth, cert=nil)
 	JSON.parse(RestClient::Request.execute(
 		method: :post,
@@ -64,10 +89,17 @@ def rest_post(payload, api_url, auth, cert=nil)
 	))
 end
 
+###################################################################
+# Method: Helper Method to correctly format scope for JSON payloads
+###################################################################
 def get_component_id_key(scope)
+	### If the scope starts with fe or be
 	scope.start_with?("fe","be") ? scope[2..-1].downcase : scope.downcase
 end
 
+########################
+# Method: API GET Method
+########################
 def rest_get(api_url, auth, cert=nil)
 	JSON.parse(RestClient::Request.execute(method: :get,
 		url: api_url,
@@ -108,7 +140,8 @@ config['unisphere'].each do |unisphere|
 				keys.each do |key|
 					metrics = get_component_metrics(unisphere,symmetrix,monitor,key,metrics_param,auth)
 					metrics_param.each do |metric|
-						metric_payload["symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{key[get_component_id_key(monitor['scope'])+'Id']}.#{metric}"] = metrics[metric]
+						metric_payload["symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{key['id']}.#{metric}"] = metrics[metric] if unisphere['version'] == 8
+						metric_payload["symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{key[get_component_id_key(monitor['scope'])+'Id']}.#{metric}"] = metrics[metric] if unisphere['version'] == 7
 					end
 				end
 			end
